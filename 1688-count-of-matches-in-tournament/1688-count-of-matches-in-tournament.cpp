39be9e36c420c20673ea4b0a634d519b54a35d88
Gourav Madhwal <49582845+GouravMadhwal@users.noreply.github.com>
2022-10-16 14:26:20 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (22.81%) - LeetHub

---
+++ 1688-count-of-matches-in-tournament/1688-count-of-matches-in-tournament.cpp
@@ -0,0 +1,22 @@
+class Solution
+{
+    public:
+        int numberOfMatches(int n)
+        {
+            int matches = 0;
+            while (n != 1)
+            {
+                if (n % 2 == 0)
+                {
+                    n = n / 2;
+                    matches = matches + n;
+                }
+                else if (n % 2 != 0)
+                {
+                    n = (n / 2) + 1;
+                    matches = matches + (n - 1);
+                }
+            }
+            return matches;
+        }
+};

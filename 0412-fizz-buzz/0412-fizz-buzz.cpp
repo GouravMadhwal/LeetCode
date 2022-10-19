362b5f81e1c89e99c10b2c3884d0efeb3b1b8d70
Gourav Madhwal <49582845+GouravMadhwal@users.noreply.github.com>
2022-10-19 15:34:50 +0530

    Time: 12 ms (10.12%), Space: 7.2 MB (97.02%) - LeetHub

--- 0412-fizz-buzz/0412-fizz-buzz.cpp
+++ 0412-fizz-buzz/0412-fizz-buzz.cpp
@@ -4,8 +4,8 @@ class Solution
         vector<string> fizzBuzz(int n)
         {
             vector<string>ans(n);
-            for(int i=0;i<n;i++){
-                ans[i] = to_string((i+1));
+            for(int i=1;i<=n;i++){
+                ans[i-1] = to_string(i);
             }
             for(int i=2;i<n;i+=3){
                 ans[i] = "Fizz";

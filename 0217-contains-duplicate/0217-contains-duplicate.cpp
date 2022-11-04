dc03b7e6be977d64ac394fe9feb2c929f9668d22
Gourav Madhwal <49582845+GouravMadhwal@users.noreply.github.com>
2022-11-04 21:52:09 +0530

    Time: 239 ms (32.39%), Space: 55 MB (7.00%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,7 +1,9 @@
-class Solution {
-public:
-    bool containsDuplicate(vector<int>& nums) {
-        unordered_set<int>ans(nums.begin(), nums.end());
-        return ans.size() != nums.size();
-    }
+class Solution
+{
+    public:
+        bool containsDuplicate(vector<int> &nums)
+        {
+            unordered_set<int> ans(nums.begin(), nums.end());
+            return ans.size() != nums.size();
+        }
 };
